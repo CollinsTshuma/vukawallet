3a64a2fa38441e38d7e4ce822135f76db5b255b2
CollinsTshuma <62089415+CollinsTshuma@users.noreply.github.com>
2022-03-13 09:10:55 +0200

    feat: Set up the React app

--- truffle-config.js
+++ truffle-config.js
@@ -2,10 +2,11 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 7545,
       network_id: "*",
     },
   },
+  contracts_build_directory: "./vukawallet/src/abis/",
   compilers: {
     solc: {
       version: "0.5.16",
